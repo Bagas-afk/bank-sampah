819e54a1cd77939f87471a03669d3563e6ff0a7b
Bagas-afk <bagasaci@gmail.com>
2020-11-20 01:38:47 +0700

    data kebalik

--- assets/js/mychart.js
+++ assets/js/mychart.js
@@ -11,7 +11,7 @@ $.ajax({
 				labels: ["Withdraw", "Setoran"],
 				datasets: [
 					{
-						data: [data[2], data[1]],
+						data: [data[1], data[2]],
 						backgroundColor: ["#4e73df", "#1cc88a", "#36b9cc"],
 						hoverBackgroundColor: ["#2e59d9", "#17a673", "#2c9faf"],
 						hoverBorderColor: "rgba(234, 236, 244, 1)",

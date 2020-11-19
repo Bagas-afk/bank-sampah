39d8bd36f862a388aaf2dea62ff9c78455de1818
Bagas-afk <bagasaci@gmail.com>
2020-11-20 01:38:22 +0700

    error topbar logout

--- application/views/templates/footer.php
+++ application/views/templates/footer.php
@@ -18,7 +18,7 @@
 </a>
 
 <!-- Logout Modal-->
-<!-- <div class="modal fade" id="logoutModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+<div class="modal fade" id="logoutModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
     <div class="modal-dialog" role="document">
         <div class="modal-content">
             <div class="modal-header">
@@ -34,7 +34,7 @@
             </div>
         </div>
     </div>
-</div> -->
+</div>
 
 <!-- Bootstrap core JavaScript-->
 <script src="<?= base_url('assets/'); ?>vendor/jquery/jquery.min.js"></script>
